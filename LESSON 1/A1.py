ad2b6b78330af227f7e90345b66d98d482c975ec
Ap3211 <suparanapatra3211@gmail.com>
2025-04-29 17:23:19 +0530

    A1

---
+++ LESSON 1/A1.py
@@ -0,0 +1,6 @@
+import coloroma
+from coloroma import Fore , Style
+from textblob import TextbBob
+coloroma init()
+print (f"{Fore.Cyan}"Welcome to Sentiment Spy! {Style.Reset_All }")
+user_name=input(f"{Fore.MAGENTA}")
